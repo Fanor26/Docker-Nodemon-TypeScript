68594e10272a095233dbe1b86fabfd83754b383f
Fanor1924 <juanorchoque90@gmail.com>
2022-05-29 06:57:47 +0000

    Server running  in  process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Typescript + Noedemon + Docker Fanor 2022 ");
+import express from 'express';
+
+const app =  express();
+
+require('dotenv').config();
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server running  in ${app.get('port')}`);
+})
+
