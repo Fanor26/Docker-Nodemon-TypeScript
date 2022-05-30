4ff177b8ef784e94222be350bb95e0a2fecbf4cb
FANOR1945 <aguirrefanor31@gmail.com>
2022-05-30 06:15:26 +0000

    db conecction solved err

--- src/database.ts
+++ src/database.ts
@@ -8,5 +8,7 @@ mongoose.connect(URI,
     useNewUrlParser: true,
     useUnifiedTopology: true
    
-}).then((db) => console.log('database connection success', db.connection.host))
+}).then((db) => console.log('database connection success', db.connection.name))
 .catch((error)=>console.log(error))
+
+module.exports= mongoose
